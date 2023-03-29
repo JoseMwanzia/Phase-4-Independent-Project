9093345e0448953deacdb39be320a88c09eb3e00
joseph mwanzia <makamwanzia@gmail.com>
2023-03-29 15:37:32 +0300

    Created a sessions controller with a landlord_login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
         landlord = Landlord.find_by(username: params[:username])
         if landlord&.authenticate(params[:password])
             session[:landlord_id] = landlord.id
-            render json: landlord
+            render json: landlord, status: :created
+        else
+            render json: { error: "Invalid username or password"}, status: :unauthorized
         end
     end
+
+    
+
 end
