2e96bbc8c5c32788aca85672c9a6ff9b0f257be0
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-29 05:47:33 -0700

    added tenant param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < ActionController::API
 
 
 
+
+
+
+
+    
+# Tenant methods
+
     def tenant 
         Tenant.find(session[:uid]).to_i
+    end
+
+    def tenant_params 
+        params.permit(:name, :email, :password,)
     end
 
 
