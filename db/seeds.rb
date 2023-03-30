ecdc9963a3bb95e2f51fd74ace78b8f365a87ba4
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-30 10:46:56 -0700

    add seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,51 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# 15.times do
+puts "💤..seed ninja...🥷"
 
-#     Tenant.create(
-#         "username": Faker:: 
-#         "email": Faker::Internet.email,
-#         "identification": Faker:: 
-#         "password_digest": Faker:: 
-#     )
+15.times do
 
-#     Landlord.create(
-#         "userrname": Faker::  
-#         "Contact_number": Faker:: 
-#         "email": Faker::Internet.email,
-#         "password_digest": Faker:: 
-#     )
+    Tenant.create(
+        "username": Faker::Name.name,
+        "email": Faker::Internet.email,
+        "identification": Faker::IDNumber.valid,
+        "password_digest": Faker::Alphanumeric.alpha(number: 5)
+    )
 
-#     Apartment.create(
-#         "name": Faker:: 
-#         "location": Faker::
-#         "type": Faker:: 
-#         "landlord_id": rand(1..15),
-#         "house_id": rand(1..15)
+   landlord = Landlord.create(
+        "username": Faker::Name.name,
+        "contact_number": Faker::PhoneNumber.cell_phone,
+        "email": Faker::Internet.email,
+        "password_digest": Faker::Alphanumeric.alpha(number: 5)
+    )
 
-#     )
+    Apartment.create(
+        "name": Faker::Lorem.words(number: 1),
+        "location": Faker::Address.full_address,
+        "category": Faker::Lorem.words(number: 1),
+        "landlord_id": landlord.id,
+        "house_id": rand(1..15)
 
-#     House.create(
-#         "image_url": Faker:: 
-#         "house_number": Faker:: 
-#         "description": Faker:: 
-#         "rent": Faker:: 
-#         "tenant_id": rand(1..15),
-#         "apartment_id": rand()
-#     )
+    )
 
-#     Review.create(
-#         "rating": rand(1..5),
-#         "description": Faker:: 
-#         "tenant_id": rand(1..15),
-#         "house_id": rand(1..15
-#     )
+    House.create(
+        "image_url": Faker::Avatar.image,
+        "house_number": Faker::Address.secondary_address,
+        "description": Faker::Lorem.sentence(word_count: 10), 
+        "rent": rand(12000.0..90000.0),
+        "apartment_id": rand(1..15)
+    )
 
-# end
+    3.times do
+        Review.create(
+        "rating": rand(1..5),
+        "description": Faker::Lorem.sentence(word_count: 10),
+        "tenant_id": rand(1..15),
+        "house_id": rand(1..15)
+     )
+    end
+
+end
+
+
+puts "🤾 . dusted. . 😅"
