38467cccc85b4bebff6d1c206a70c695290c349d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-30 10:47:18 -0700

    add authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,17 +13,29 @@ class ApplicationController < ActionController::API
 
     def tenant_authorize 
         # Tenant.find(session[:uid]).to_i
-        return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include: :tid
+        return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :tid
     end
 
     def tenant_params 
-        params.permit(:name, :email, :password,)
+        params.permit(:id, :username, :email, :password)
     end
 
 #  Landlord 
 
 def landlord_authorize
-    return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include: :lid
+    return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :lid
+end
+
+
+def landlord_params
+    params.permit(:id, :username, :contact_number, :email, :password)
+end
+
+
+# rescue all common errors
+
+def standard_error(exception)
+    render json: {message: "Failed", data: {info: exception.message}}, status: :unprocessable_entity
 end
 
 
